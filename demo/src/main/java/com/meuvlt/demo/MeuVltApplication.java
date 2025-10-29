a23486b355d3b2c356d230654dcfa38335f9d585
joaooliveiradev1 <joaooliveiradev1@gmail.com>
2025-10-29 01:09:48 -0300

    Startando projeto springboot

---
+++ demo/src/main/java/com/meuvlt/demo/MeuVltApplication.java
@@ -0,0 +1,13 @@
+package com.meuvlt.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuVltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuVltApplication.class, args);
+	}
+
+}
