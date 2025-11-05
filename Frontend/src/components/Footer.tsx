2612f2eda7a8bd9aef473612ea58b22e6d2bfe99
joaooliveiradev1 <joaooliveiradev1@gmail.com>
2025-11-05 08:47:24 -0300

    feat: startando front end no repositorio monolitico a partir da ultima branch antes do conflito no repositorio anterior branch:frontend-jc

---
+++ Frontend/src/components/Footer.tsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="py-7 text-center text-gray-600 text-sm bg-white border-t border-gray-200">
+      © 2025 Meu VLT — Projeto acadêmico UNIFOR. Todos os direitos reservados.
+    </footer>
+  );
+}
