95d3286be1f7784ada1ffc42798355d8f1ae62c9
oliveiradev <joaooliveiradev1@gmail.com>
2025-11-06 21:55:38 -0300

    m

--- Frontend/src/services/auth.ts
+++ Frontend/src/services/auth.ts
@@ -1,6 +1,6 @@
 export async function login(email: string, senha: string) {
   try {
-    const response = await fetch("http://localhost:8080/auth/login", {
+    const response = await fetch("/auth/login", {
       method: "POST",
       headers: {
         "Content-Type": "application/json"
